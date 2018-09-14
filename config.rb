5ec8f0988566c73c3c9cbccf67353bd8dd206073
lestephen <lestephen@groupon.com>
2018-09-13 17:13:26 -0700

    Update LBB

--- config.rb
+++ config.rb
@@ -3,7 +3,8 @@ OotpDownload.configure do |c|
   c.add League.new :CBL, 'http://www.thecblonline.com/zips/cbl.zip', :create_league_directory => true
   c.add League.new :GABL, 'http://www.goldenageofbaseball.com/commish/gabl.lg.zip'
   c.add League.new :HFTC, 'http://www.hitforthecycle.com/Hit%20For%20The%20Cycle.zip'
-  c.add League.new :LBB, 'http://bbs56.net/LBB/file/LBB.lg.zip'
+  #c.add League.new :LBB, 'http://bbs56.net/LBB/file/LBB.lg.zip'
+  c.add League.new :LBB, 'http://www.hoffbl.com/lbb/LBB.lg.zip'
   c.add League.new :SAVOY, 'http://www.thecblonline.com/savoy/zips/Savoy.zip', :create_league_directory => true
 
   c.add League.new :TWML, 'http://www.darowski.com/twml/OOTP6Reports/TWML%202.0.lg.zip', :version => :OOTP6
