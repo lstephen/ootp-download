217bfbc30e5cdf608a2474265b84eac50cadda8b
lestephen <lestephen@groupon.com>
2018-10-30 07:58:45 -0700

    Update TWML config

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ OotpDownload.configure do |c|
   c.add League.new :LBB, 'http://www.hoffbl.com/lbb/LBB.lg.zip'
   c.add League.new :SAVOY, 'http://www.thecblonline.com/savoy/zips/Savoy.zip', :create_league_directory => true
 
-  c.add League.new :TWML, 'http://www.darowski.com/twml/OOTP6Reports/TWML%202.0.lg.zip', :version => :OOTP6
+  c.add League.new :TWML, 'http://www.darowski.com/twml/OOTP6Reports/TWML%202.0.lg.zip', :version => :OOTP6, :create_league_directory => true
 
   c.add League.new :BTH, 'http://bthbaseball.allsimbaseball10.com/game/lgexports/BTH.lg.zip', :version => :OOTP65
   c.add League.new :OLDBTH, 'bthbaseball.allsimbaseball10.com/game/oldbth/oldBTH.lg.zip', :version => :OOTP65
